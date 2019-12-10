076ef6562366d2475db2afd426ab19c647f20652
vishalbagde <vishalbagde.mmm@gmail.com>
2019-12-10 12:33:55 +0530

    edit lamdaexpression file

--- src/main/java/demo/LamdaExpExample1.java
+++ src/main/java/demo/LamdaExpExample1.java
@@ -43,6 +43,8 @@ public class LamdaExpExample1 {
 		
 		LamdaExpExample1.MasterClassLambda l  =new LamdaExpExample1().new MasterClassLambda();
 		l.toString();
+		System.out.println("hellow world");
+		l.toString();
 	
 		
 	}
